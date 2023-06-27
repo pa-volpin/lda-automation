bf4c5d232343f4dc17ba729ab8de49dc7d654496
pa-volpin <paulo.volpin@gmail.com>
2023-06-27 20:51:38 -0300

    Change vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    "global": {},
-  },
+  resolve: {
+  		alias: {
+			"./runtimeConfig": "./runtimeConfig.browser"
+		}
+	}
 })
